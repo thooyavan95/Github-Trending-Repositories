347a4ee1b8d622e4a21bacec193c69c39836f169
thooyavan <thooyavan1795@gmail.com>
2021-07-03 20:21:26 +0530

    Initial commit

---
+++ app/src/main/java/com/thooyavan95/githubtrendingrepositories/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thooyavan95.githubtrendingrepositories
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
