55ed1209a6f58d1484f691692f7321cb7ebf94dc
thooyavan <thooyavan1795@gmail.com>
2021-07-04 01:35:24 +0530

    implementation for recycler view adapter and view holder added

---
+++ app/src/main/java/com/thooyavan95/githubtrendingrepositories/ui/adapter/RepositoryAdapter.kt
@@ -0,0 +1,28 @@
+package com.thooyavan95.githubtrendingrepositories.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.thooyavan95.githubtrendingrepositories.entity.Repo
+
+class RepositoryAdapter : RecyclerView.Adapter<RepositoryItemViewHolder>() {
+
+    private val repoList : MutableList<Repo> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoryItemViewHolder {
+        return RepositoryItemViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: RepositoryItemViewHolder, position: Int) {
+        holder.bind(repoList[position])
+    }
+
+    override fun getItemCount(): Int {
+
+        return if(repoList.isEmpty()) 0 else repoList.size
+    }
+
+    fun updateRepoList(listOfRepos : List<Repo>){
+        repoList.addAll(listOfRepos)
+    }
+
+}
---
+++ app/src/main/java/com/thooyavan95/githubtrendingrepositories/ui/adapter/RepositoryItemViewHolder.kt
@@ -0,0 +1,29 @@
+package com.thooyavan95.githubtrendingrepositories.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.thooyavan95.githubtrendingrepositories.R
+import com.thooyavan95.githubtrendingrepositories.databinding.ItemRepoBinding
+import com.thooyavan95.githubtrendingrepositories.entity.Repo
+
+class RepositoryItemViewHolder(private val binding : ItemRepoBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(repo : Repo){
+
+        binding.itemRepoId.text = repo.id.toString()
+        binding.itemRepoName.text = repo.name
+    }
+
+    companion object{
+
+        fun create(parent : ViewGroup) : RepositoryItemViewHolder{
+
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_repo, parent, false)
+            val binding = ItemRepoBinding.bind(view)
+            return RepositoryItemViewHolder(binding)
+        }
+    }
+
+}
